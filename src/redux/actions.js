599d6fa2e484726ff60d0e4c81ee621facd339e2
Md Aquib <96131970+MdAquib388@users.noreply.github.com>
2024-05-14 18:19:45 +0530

    Add files via upload

---
+++ src/redux/actions.js
@@ -0,0 +1,10 @@
+// actions.js
+export const addTask = (task) => ({
+    type: "ADD_TASK",
+    payload: task,
+  });
+  
+  export const deleteTask = (taskId) => ({
+    type: "DELETE_TASK",
+    payload: taskId,
+  });
